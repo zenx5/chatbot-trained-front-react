49babd24507c11016d758c256b068e87d0a89d62
Octavio Martinez <omartinez1618@gmail.com>
2023-06-02 00:34:36 -0400

    refactor(App.js): remove unused imports and logo.svg feat(App.js): add react-router-dom and ChatView component feat(index.js): add HashRouter and ProviderChat components to App

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+import { ProviderChat } from './tools/ConversationProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <ProviderChat>
+        <App />
+      </ProviderChat>
+    </HashRouter>
   </React.StrictMode>
 );
 
